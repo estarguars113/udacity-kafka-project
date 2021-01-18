e9995961286c720a29035f2aa72a441ec2e7951a
paolagc <estarguars113@gmail.com>
2021-01-18 08:46:06 -0500

    Added support for optional fields

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -13,18 +13,32 @@ SCHEMA_REGISTRY_URL = "http://localhost:8081"
 BROKER_URL = "PLAINTEXT://localhost:9092"
 
 class Producer:
+    """Producer base-class providing common utilites and functionality"""
+import logging
+import time
+
+from confluent_kafka import avro
+from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka.avro import AvroProducer, CachedSchemaRegistryClient
+
+logger = logging.getLogger(__name__)
+SCHEMA_REGISTRY_URL = "http://localhost:8081"
+BROKER_URL = "PLAINTEXT://localhost:9092"
+
+
+class Producer:
     """Defines and provides common functionality amongst Producers"""
 
     # Tracks existing topics across all Producer instances
     existing_topics = set([])
 
     def __init__(
-        self,
-        topic_name,
-        key_schema,
-        value_schema=None,
-        num_partitions=1,
-        num_replicas=1,
+            self,
+            topic_name,
+            key_schema,
+            value_schema=None,
+            num_partitions=1,
+            num_replicas=1,
     ):
         """Initializes a Producer object with basic settings"""
         self.topic_name = topic_name
@@ -33,12 +47,8 @@ class Producer:
         self.num_partitions = num_partitions
         self.num_replicas = num_replicas
 
-        # initialize service registry
-        schema_registry = CachedSchemaRegistryClient(SCHEMA_REGISTRY_URL)
-        
-        # initialize broker
         self.broker_properties = {
-            "bootstrap.servers": BROKER_URL,
+            'bootstrap.servers': BROKER_URL
         }
 
         # If the topic does not already exist, try to create it
@@ -48,43 +58,26 @@ class Producer:
 
         self.producer = AvroProducer(
             self.broker_properties,
-            schema_registry=schema_registry,
+            schema_registry=CachedSchemaRegistryClient(SCHEMA_REGISTRY_URL),
             default_key_schema=self.key_schema,
             default_value_schema=self.value_schema
         )
-        
-        
+
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
-        futures = AdminClient(self.broker_properties).create_topics([
-                NewTopic(
-                    self.topic_name,
-                    num_partitions=self.num_partitions,
-                    replication_factor=self.num_replicas,
-                    config={
-                        'cleanup.policy': 'compact',
-                        'delete.retention.ms': 2000,
-                        'file.delete.delay.ms': 2000
-                    }
-                )
-            ])
-
-        for topic, future in futures.items():
-            print('topic', topic, future)
-            try:
-                future.result()
-                print("topic created")
-            except Exception as e:
-                print(f"failed to create topic {topic.topic_name}: {e}")
-                raise
-
-            
-
-    def time_millis(self):
-        return int(round(time.time() * 1000))
+        AdminClient(self.broker_properties).create_topics(
+            [NewTopic(
+                self.topic_name, 
+                num_partitions=self.num_partitions,
+                replication_factor=self.num_replicas
+            )])
 
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
-        if self.producer:
+        if self.producer is not None:
             self.producer.flush()
+
+    def time_millis(self):
+        """Use this function to get the key for Kafka Events"""
+        return int(round(time.time() * 1000))
 
